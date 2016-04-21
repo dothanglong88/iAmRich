f9ddb1312c9a6b0b4b35991a4cdc3206ca336801
Xmob - Longdt <longdt@Xmobs-MacBook-Pro.local>
2016-04-21 18:08:33 +0700

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+iAmRich written by Long
---
+++ iAmRich/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iAmRich
+//
+//  Created by Xmob - Longdt on 4/19/16.
+//  Copyright © 2016 Xmob - Longdt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
